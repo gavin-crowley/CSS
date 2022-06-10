b27b4ba7af6012faacfbdb3e1f554d5e5b47bc6f
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-06-10 18:36:34 +0100

    navigation

---
+++ Navigation/cem-flex-nav/script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById('toggleButton')
+const naviList = document.getElementById('navi-list')
+
+toggleButton.addEventListener('click', () => {
+    naviList.classList.toggle('active')
+})
